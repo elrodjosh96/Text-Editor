94d87f150c816e7d02dad72bc2966d1e8d9ab66c
Josh Elrod <elrodjosh96@gmail.com>
2022-01-15 10:44:29 -0500

    fixed id error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,14 +14,13 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
   const jateDb = await openDB('jate', 1);
 
   const tx = jateDb.transaction('jate', 'readwrite');
 
   const store = tx.objectStore('jate');
 
-  const request = store.put({ id: id, content: content });
+  const request = store.put({ id: 1, content: content });
 
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -29,7 +28,6 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   const jateDb = await openDB('jate', 1);
 
   const tx = jateDb.transaction('jate', 'readonly');
@@ -40,7 +38,7 @@ export const getDb = async () => {
 
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value 
 };
 
 initdb();
